3982e9682e7e37e20e6fa86c188d3d03e4e57bf6
jkp09x <jkp09x@users.noreply.github.com>
2020-06-09 21:13:39 -0400

    Adding for loop syntax with fizzbuzz example

--- index.js
+++ index.js
@@ -153,4 +153,22 @@ function fizzBuzzWhile(n) {
   console.log(output);
 }
 
+function fizzBuzzFor(n) {
+  var output = [];
+
+  for (var count = 1; count <= n; count++) {
+    if ((count % 3) === 0 && (count % 5) === 0) {
+      output.push("FizzBuzz");
+    } else if (count % 5 === 0) {
+      output.push("Buzz");
+    } else if (count % 3 === 0) {
+      output.push("Fizz");
+    } else {
+      output.push(count);
+    }
+  }
+  console.log(output);
+}
+
 fizzBuzzWhile(100);
+fizzBuzzFor(100);
